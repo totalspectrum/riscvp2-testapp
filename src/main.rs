94226fe60759600649f0480d2fac33266b286372
Eric Smith <ersmith@totalspectrum.ca>
2024-07-10 17:10:34 -0300

    added serial character output

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,14 @@ use riscvp2_rt::*;
 
 #[no_mangle]
 pub fn main() -> ! {
+    let mut ch : u8 = b'A';
     loop {
       pintoggle(57);
       waitms(200);
+      tx(ch);
+      ch = ch + 1;
+      if ch > b'Z' {
+        ch = b'A';
+      }
     }
 }
